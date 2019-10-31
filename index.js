10c40b50107fc0d3d5014eeec210cf749f16efc1
umekow <umekolashun@gmail.com>
2019-10-31 16:48:45 -0500

    header

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 5000;
 server.use(express.json()); 
 server.use('/api/posts', dbRouter);
 
-
+server.get('/', (req, res) => {
+   res.send(`
+   <h2>Deployed a[[</h2>
+   <p>Welcome${nameInsert} to Umeko's First Deployed Backend</p>
+   `)
+})
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
